3ce1e9945e24a9e7d2df6ccae8f5d20a82bfbc2c
Vitaliy <pugachev.vt@gmail.com>
2023-02-06 21:50:59 +0300

    first_practic

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
-console.log(x);
-var x = 10;
+let numberOfFilms = +prompt("How many films have you watched?","");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+const   a = prompt("What was the last movie you watched?",""),
+        b = +prompt("Rate the movie", 0);
+        c = prompt("What was the last movie you watched?",""),
+        d = +prompt("Rate the movie", 0);
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
